b8f473d656946bc6da17efb61d2a51a1d42ae1ea
u1d4l <54950086+u1d4l@users.noreply.github.com>
2020-05-27 22:14:30 -0300

    About is working ok Random plot is not pretty

--- about.py
+++ about.py
@@ -7,18 +7,25 @@ def show():
     about_win = tk.Tk()
     about_win.title("About microtax")
     about_win.resizable(False, False)
-    ttk.Label(about_win, text="\tVersion\n").grid(column=0, row=0)
-    ttk.Label(about_win, text="\tLicence\n").grid(column=0, row=1)
-    ttk.Label(about_win, text="\tBy\n").grid(column=0, row=2)
-    githublink = ttk.Button(about_win, text="Github", command=webbrowser.open_new(r"https://github.com/u1d4lp.html"))
+    ttk.Label(about_win, text="Version").grid(column=0, row=0)
+    ttk.Label(about_win, text="Licence").grid(column=0, row=1)
+    ttk.Label(about_win, text="By").grid(column=0, row=2)
+    githublink = ttk.Button(about_win, text="Github", command=open_github_link)
     githublink.grid(column=0, row=3)
-    linkedinlink = ttk.Button(about_win, text="LinkedIn", command=webbrowser.open_new(r"https://www.linkedin.com/in/paulo-vidal-30b160163/"))
-    linkedinlink.grid(column=0, row=4)
-    ttk.Label(about_win, text="\t1.0\t\n").grid(column=1, row=0)
-    ttk.Label(about_win, text="\tMIT Licence\t\n").grid(column=1, row=1)
-    ttk.Label(about_win, text="\tPaulo Vidal\t\n").grid(column=1, row=2)
+    linkedinlink = ttk.Button(about_win, text="LinkedIn", command=open_linkedin_link)
+    linkedinlink.grid(column=1, row=3)
+    ttk.Label(about_win, text="0.9").grid(column=1, row=0)
+    ttk.Label(about_win, text="MIT Licence").grid(column=1, row=1)
+    ttk.Label(about_win, text="Paulo Vidal").grid(column=1, row=2)
     about_win.mainloop()
 
 
 def manual():
     os.startfile("manual.html")
+
+
+def open_github_link():
+    webbrowser.open_new(r"https://github.com/u1d4lp.html")
+
+def open_linkedin_link():
+    webbrowser.open_new(r"https://www.linkedin.com/in/paulo-vidal-30b160163/")
