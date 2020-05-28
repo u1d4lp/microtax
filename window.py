1f663a2e3702b0c3858838c01061ae6d9f2a1c4d
u1d4l <54950086+u1d4l@users.noreply.github.com>
2020-05-28 11:30:15 -0300

    Interface simplified

--- window.py
+++ window.py
@@ -32,48 +32,43 @@ menu_bar.add_cascade(label="Help", menu=help_menu)
 
 # Labels, column = 0
 
-ttk.Label(main_win, text="\tProduct maximum demand\n").grid(column=0, row=1)
-ttk.Label(main_win, text="\tDemand curve elasticity\n").grid(column=0, row=2)
-ttk.Label(main_win, text="\n\tEnter supply definitions\n").grid(column=0, row=3)
-ttk.Label(main_win, text="\tProduct minimum supply\n").grid(column=0, row=4)
-ttk.Label(main_win, text="\tSupply curve elasticity\n").grid(column=0, row=5)
-ttk.Label(main_win, text="\n\tEnter tax options\n").grid(column=0, row=6)
-ttk.Label(main_win, text="\tFixed tax per product\n").grid(column=0, row=7)
-ttk.Label(main_win, text="\tProportional tax per $1\n").grid(column=0, row=8)
-
-# Empty column
-ttk.Label(main_win, text="     ").grid(column=1, row=0)
+ttk.Label(main_win, text="Product maximum demand").grid(column=0, row=0)
+ttk.Label(main_win, text="Demand curve elasticity").grid(column=0, row=1)
+ttk.Label(main_win, text="Product minimum supply").grid(column=0, row=2)
+ttk.Label(main_win, text="Supply curve elasticity").grid(column=0, row=3)
+ttk.Label(main_win, text="Fixed tax per product").grid(column=0, row=4)
+ttk.Label(main_win, text="Proportional tax per $1").grid(column=0, row=5)
 
 
 # Textbox widgets, column = 1
 demand_max = tk.IntVar()
 enter_demand_max = ttk.Entry(main_win, width=12, textvariable=demand_max)
-enter_demand_max.grid(column=1, row=1)
+enter_demand_max.grid(column=1, row=0)
 
 demand_el = tk.DoubleVar()
 enter_demand_el = ttk.Entry(main_win, width=12, textvariable=demand_el)
-enter_demand_el.grid(column=1, row=2)
+enter_demand_el.grid(column=1, row=1)
 
 supply_min = tk.IntVar()
 enter_supply_min = ttk.Entry(main_win, width=12, textvariable=supply_min)
-enter_supply_min.grid(column=1, row=4)
+enter_supply_min.grid(column=1, row=2)
 
 supply_el = tk.DoubleVar()
 enter_supply_el = ttk.Entry(main_win, width=12, textvariable=supply_el)
-enter_supply_el.grid(column=1, row=5)
+enter_supply_el.grid(column=1, row=3)
 
 fix_tax = tk.DoubleVar()
 enter_fix_tax = ttk.Entry(main_win, width=12, textvariable=fix_tax)
-enter_fix_tax.grid(column=1, row=7)
+enter_fix_tax.grid(column=1, row=4)
 
 prop_tax = tk.DoubleVar()
 enter_prop_tax = ttk.Entry(main_win, width=12, textvariable=prop_tax)
-enter_prop_tax.grid(column=1, row=8)
+enter_prop_tax.grid(column=1, row=5)
 
 # Apply button
 apply_action = ttk.Button(main_win, text="Plot", command=plotting.show_plt)
-apply_action.grid(column=0, row=9)
+apply_action.grid(column=0, row=6)
 
 # Random button
 random_action = ttk.Button(main_win, text="Randomize", command=plotting.random_plt)
-random_action.grid(column=1, row=9)
+random_action.grid(column=1, row=6)
