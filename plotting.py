4a638fae586714fd21e3b4b7574fd26ae8c172c8
u1d4l <54950086+u1d4l@users.noreply.github.com>
2020-05-27 21:26:25 -0300

    Window.py variable names corrected Plot equations corrected Random plotting working fully

--- plotting.py
+++ plotting.py
@@ -13,10 +13,10 @@ def show_plt():
     ax.set_xlabel('Quantity\n')
     ax.set_ylabel('Price')
     ax.grid(True)
-    quantity = np.linspace(window.supply_1.get(), window.demand_1.get(), 100000)
-    demand_curve = plt.plot(quantity, (-quantity + window.demand_1.get())/window.demand_el.get()) #azul
-    supply_curve = plt.plot(quantity, (quantity - window.supply_1.get())/window.supply_el.get()) #laranja
-    tax_supply_curve = plt.plot(quantity, (quantity + window.prop_tax.get() * (window.fix_tax.get() - window.supply_1.get())/window.supply_el.get())) #verde
+    quantity = np.linspace(window.supply_min.get(), window.demand_max.get(), 100000)
+    demand_curve = plt.plot(quantity, (-quantity + window.demand_max.get())/window.demand_el.get()) #azul
+    supply_curve = plt.plot(quantity, (quantity - window.supply_min.get())/window.supply_el.get()) #laranja
+    tax_supply_curve = plt.plot(quantity, (quantity + (1+window.prop_tax.get()) * (window.fix_tax.get() - window.supply_min.get())/window.supply_el.get())) #verde
     plt.show()
 
 
@@ -27,14 +27,14 @@ def random_plt():
     ax.set_xlabel('Quantity\n')
     ax.set_ylabel('Price')
     ax.grid(True)
-    demand_1 = random.uniform(100, 500)
+    demand_max = random.uniform(100, 500)
     demand_el = random.uniform(0.1, 0.9)
-    supply_1 = random.uniform((demand_1 / 2), (demand_1 / 5))
+    supply_min = random.uniform((demand_max / 2), (demand_max / 5))
     supply_el = random.uniform(0.1, 0.9)
     fix_tax = random.uniform(0.5, 10)
     prop_tax = random.uniform(0.01, 0.9)
-    quantity = np.linspace(0, window.demand_1.get(), 100000)
-    demand_curve = plt.plot(quantity, (-quantity + window.demand_1.get()) / window.demand_el.get())
-    # supply_curve =
-    # tax_supply_curve =
+    quantity = np.linspace(supply_min, demand_max, 100000)
+    demand_curve = plt.plot(quantity, (-quantity + demand_max) / demand_el)  # azul
+    supply_curve = plt.plot(quantity, (quantity - supply_min) / supply_el)  # laranja
+    tax_supply_curve = plt.plot(quantity, (quantity + (1+prop_tax) * (fix_tax - supply_min) / supply_el))  # verde
     plt.show()
