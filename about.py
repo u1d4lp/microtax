6a2362f4c8162e14342d010ee0d9156fe06f974f
u1d4l <54950086+u1d4l@users.noreply.github.com>
2020-05-27 21:46:34 -0300

    About is broken, but links work Random plot is not pretty

--- about.py
+++ about.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import webbrowser
 import os
 
 def show():
@@ -9,13 +10,13 @@ def show():
     ttk.Label(about_win, text="\tVersion\n").grid(column=0, row=0)
     ttk.Label(about_win, text="\tLicence\n").grid(column=0, row=1)
     ttk.Label(about_win, text="\tBy\n").grid(column=0, row=2)
-    ttk.Label(about_win, text="\tGithub\n").grid(column=0, row=3)
-    ttk.Label(about_win, text="\tLinkedIn\n").grid(column=0, row=4)
+    githublink = ttk.Button(about_win, text="Github", command=webbrowser.open_new(r"https://github.com/u1d4lp.html"))
+    githublink.grid(column=0, row=3)
+    linkedinlink = ttk.Button(about_win, text="LinkedIn", command=webbrowser.open_new(r"https://www.linkedin.com/in/paulo-vidal-30b160163/"))
+    linkedinlink.grid(column=0, row=4)
     ttk.Label(about_win, text="\t1.0\t\n").grid(column=1, row=0)
     ttk.Label(about_win, text="\tMIT Licence\t\n").grid(column=1, row=1)
     ttk.Label(about_win, text="\tPaulo Vidal\t\n").grid(column=1, row=2)
-    ttk.Label(about_win, text="\thttps://github.com/u1d4lp\t\n").grid(column=1, row=3)
-    ttk.Label(about_win, text="\thttps://www.linkedin.com/in/paulo-vidal-30b160163/\t\n").grid(column=1, row=4)
     about_win.mainloop()
 
 
