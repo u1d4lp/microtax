a9c61e04c18ba22b16b54b0d796cbac0871631fd
u1d4l <54950086+u1d4l@users.noreply.github.com>
2020-05-29 22:37:53 -0300

    Clear function corrected

--- file.py
+++ file.py
@@ -13,9 +13,9 @@ def save():
 
 
 def clear():
-    window.enter_demand_1.delete('0',tk.END)
+    window.enter_demand_max.delete('0',tk.END)
     window.enter_demand_el.delete('0', tk.END)
-    window.enter_supply_1.delete('0', tk.END)
+    window.enter_supply_min.delete('0', tk.END)
     window.enter_supply_el.delete('0', tk.END)
     window.enter_fix_tax.delete('0', tk.END)
     window.enter_prop_tax.delete('0', tk.END)
